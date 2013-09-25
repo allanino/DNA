a3747ae46a6ea5cc6dca4a27a6211f07b115d594
Allan Costa <allaninocencio@yahoo.com.br>
2013-09-24 22:02:24 -0300

    Dictionary created. Time to code[

--- dna.py
+++ dna.py
@@ -3,17 +3,42 @@ import csv
 
 code = {}
 
-line = open("data/huff3_code.dat", "r").read().split()
-print line
+# line = open("data/huff3_code.dat", "r").read().split()
 
-for i in range(0, 257, 6):
-    code[line[i]] = line[i]
 
-for s in code:
-    print s
+csv_reader = csv.reader(open("data/huff3_code.dat", "r"))
 
-for i in range(0,257):
-    print "%d: %s" % (i, code[str(i)])
+i = 0
+for line in csv_reader:
+    row = line[0].split()
+    try: 
+        if code[row[0]]:
+            print "*" * 10, row[0]
+    except Exception:
+        print 'fudeu'
+    finally:
+        code[row[0]] = row[1]
+    i = i + 1
+
+print i
+
+# for i in range(0, len(line), 6):
+#     code[line[i]] = line[i+1]
+
+l = []
+print len(code)
+for s in code.keys():
+    print "%s: %r" % (s, code[s])
+    l.append(int(s))
+
+print sorted(l), len(l)
+print 
+
+f = open("data/huff3.dict", "w+")
+for code, key in sorted([(v,k) for k,v in code.iteritems()], key=lambda tup: int(tup[1])):
+    f.write("%s,%s\n" %  (key,code))
+# for i in range(0,257):
+#     print "%d: %r" % (i, code[str(i)])
 
 # with open("MLK_excerpt_VBR_45-85.mp3", "rb") as f:
 #     byte = f.read(1)
@@ -21,3 +46,4 @@ for i in range(0,257):
 #         print "%r" %  binascii.hexlify(byte), 
 #         print "%r" %  int(binascii.hexlify(byte), 16)
 #         byte = f.read(1)
+
