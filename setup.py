7866ecdb802507657e4b3a61bf34703bf68b6e17
allanino <allaninocencio@yahoo.com.br>
2014-09-04 17:50:26 -0300

    Setup file to install console script

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='DNA',
+    version='0.1',
+    author = 'Allan Inocencio de Souza Costa',
+    author_email = 'allaninocencio@yahoo.com.br',
+    description = 'A script o encode/decode arbitrary computer files into DNA sequences.',
+    packages=['dna',],
+    license='MIT',
+    keywords= 'dna encoding decoding file',
+    long_description=read('README.md'),
+    entry_points = {
+        'console_scripts': ['dna=dna.dna:main'],
+    }
+)
