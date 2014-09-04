37f5d7e0648bbc4a6682395ab213c87965c12c23
allanino <allaninocencio@yahoo.com.br>
2014-09-04 18:11:11 -0300

    Fix build errors because of README extension

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def read(fname):
 
 setup(
     name='file2dna',
-    version='0.1',
+    version='0.2',
     author = 'Allan Inocencio de Souza Costa',
     author_email = 'allaninocencio@yahoo.com.br',
-    description = 'A script o encode/decode arbitrary computer files into DNA sequences.',
+    description = 'A script to encode/decode arbitrary computer files into DNA sequences.',
     packages=['dna',],
     license='MIT',
     keywords= 'dna encoding decoding file',
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     entry_points = {
         'console_scripts': ['dna=dna.dna:main'],
     }
